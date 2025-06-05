2cb7864b9fadf905bec997a71a5ab8ee9b770c1b
rohan908 <145878828+rohan908@users.noreply.github.com>
2025-06-05 15:31:50 -0400

    horizontal side done

--- src/states/7-HorizontalSide.cpp
+++ src/states/7-HorizontalSide.cpp
@@ -5,10 +5,31 @@ void HorizontalSide::initialize_impl() {
 }
 
 State *HorizontalSide::loop_impl() {
-    Serial.println("Horizontal Side looped");
-    
-    if (this->currentTime > 5000) {
-        return (State *)new ExtendAuger(this->ctx);
+  Serial.println("Horizontal Side looped");
+  ctx->horzFlap.write(FLAP_EXTENDED_POS); //money buys pid :) i love axons
+
+  const auto gyroData = ctx->accel.getData();
+  if (gyroData.getLastUpdated() != lastGyroReadTime) {
+      lastGyroReadTime = gyroData.getLastUpdated();
+      if (isRotatingDebouncer.update(abs(gyroData->gyrZ) > IS_TUMBLING_VEL_THRESHOLD, //TODO: check that axis is correct
+                                      ::millis())) {
+          return new Tumbling(ctx);
+      }
+  }
+
+  //if we are stuck flapping and we aren't rotating then we are in a bush bruh... BEGIN FLAILING BC ITS SO OVER
+  if (this->currentTime > MAX_TRY_BEFORE_FLAIL_TIME) {
+    if (ctx->inBushTimesFlailed < 4) {
+      ctx->inBushTimesFlailed += 1;
+      return new VerticalSide(ctx);
     }
-    return nullptr;
+    else { //ik i dont need an else here but i like readability
+      //we've reached max flail tries on both sets of flaps so we give up
+      return new SolidDelivery(ctx);
+    }
+  }
+
+
+  return nullptr;
 }
+
