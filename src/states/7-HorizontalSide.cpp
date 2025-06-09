69c4b7305b3e031c81d86ee8527a018da3f351c8
rohan908 <145878828+rohan908@users.noreply.github.com>
2025-06-09 14:51:36 -0500

    added gyZBias shi to vertical and horizontal side flapping

--- src/states/7-HorizontalSide.cpp
+++ src/states/7-HorizontalSide.cpp
@@ -11,7 +11,7 @@ State *HorizontalSide::loop_impl() {
   const auto gyroData = ctx->accel.getData();
   if (gyroData.getLastUpdated() != lastGyroReadTime) {
       lastGyroReadTime = gyroData.getLastUpdated();
-      if (isRotatingDebouncer.update(std::abs(gyroData->gyrZ) > IS_TUMBLING_VEL_THRESHOLD, //TODO: check that axis is correct
+      if (isRotatingDebouncer.update(std::abs(gyroData->gyrZ - ctx->gyZBias) > IS_TUMBLING_VEL_THRESHOLD, //TODO: check that axis is correct
                                       ::millis())) {
           return new Tumbling(ctx);
       }
